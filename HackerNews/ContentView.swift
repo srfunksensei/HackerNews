f7fcc9842cf8e6767e640658b581edaeb54cbab4
srfunksensei <sr.funk.sensei@gmail.com>
2020-09-23 10:14:10 +0200

    initial commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by MB on 21/09/2020.
+//  Copyright © 2020 MB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# HackerNews
+
+Building an app which will present latest Hacker News. App presents content in browser. Tutorial driven.
+
+## Built with
+
+- [XCode 11.6](https://developer.apple.com/xcode/)
+- [Swift 5](https://developer.apple.com/swift/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
